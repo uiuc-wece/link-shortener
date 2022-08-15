9f07c099941a4152e2b45c815c018a3299c644ac
estrella <epopo2@illinois.edu>
2022-08-15 17:58:58 -0500

    Fix for Vercel

--- config/db.js
+++ config/db.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
+        await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         });
         console.log('MongoDB Connected...');
@@ -14,4 +12,4 @@ const connectDB = async () => {
     }
     }
 
-    module.exports = connectDB;
+module.exports = connectDB;
